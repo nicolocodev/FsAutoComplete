b7685d00d63207c4da78604ca105187733f1286e
Alan Barr <alan@alanmbarr.com>
2018-03-04 17:51:03 -0600

    update test references

--- src/FsAutoComplete/AssemblyInfo.fs
+++ src/FsAutoComplete/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A command line tool for interfacing with FSharp.Compiler.Service over a pipe.")>]
 [<assembly: AssemblyVersionAttribute("0.34.0")>]
 [<assembly: AssemblyFileVersionAttribute("0.34.0")>]
-[<assembly: AssemblyMetadataAttribute("githash","887cc2b5bc6a4fc56e8bbfc8646bdad329b0d888")>]
+[<assembly: AssemblyMetadataAttribute("githash","6d6e953b2928ca990adb6c773acde4d1c24ab7a8")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,4 +16,4 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyDescription = "A command line tool for interfacing with FSharp.Compiler.Service over a pipe."
     let [<Literal>] AssemblyVersion = "0.34.0"
     let [<Literal>] AssemblyFileVersion = "0.34.0"
-    let [<Literal>] AssemblyMetadata_githash = "887cc2b5bc6a4fc56e8bbfc8646bdad329b0d888"
+    let [<Literal>] AssemblyMetadata_githash = "6d6e953b2928ca990adb6c773acde4d1c24ab7a8"
